bdebfb9fe7c5c51cef11eecf8401846a80305039
chronolai <x941i6c04@gmail.com>
2020-07-02 18:16:08 +0000

    Deploying to gh-pages from  @ 463570e52d51229a2f941b3524639fb20d558e5a 🚀

---
+++ precache-manifest.b700a3cc97b922355ee320e15b659f20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaf169c67bf3cbf86dbff85e308ac294",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bda000bc1dde6d2bd6a3",
+    "url": "/static/css/main.337bc9fa.chunk.css"
+  },
+  {
+    "revision": "c516dd9872c5b010aa53",
+    "url": "/static/js/2.c53b2b1a.chunk.js"
+  },
+  {
+    "revision": "dcde9f1c65051fbe47b368bf76d32124",
+    "url": "/static/js/2.c53b2b1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bda000bc1dde6d2bd6a3",
+    "url": "/static/js/main.b67767a9.chunk.js"
+  },
+  {
+    "revision": "46e8a39c1186cd971e46",
+    "url": "/static/js/runtime-main.70dd68b4.js"
+  },
+  {
+    "revision": "aa14f98c377e66e7a58312b46dcb142f",
+    "url": "/static/media/52poke.aa14f98c.png"
+  }
+]);
